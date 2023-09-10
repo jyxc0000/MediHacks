6773b6bdd02a46a53b23b3f8cc60cfd924f52746
jyxc0000 <123103171+jyxc0000@users.noreply.github.com>
2023-09-10 09:41:16 -0700

    a

---
+++ profile.js
@@ -0,0 +1,4 @@
+document.body.innerHTML = "<br /> <br /> <br /> <br /> <br /> <br />Full Name: " + localStorage.getItem("Is_first_name") + " " + localStorage.getItem("Is_last_name") + "<br /> Email: " + localStorage.getItem("Is_email") + "<br /> Password: " + localStorage.getItem("Is_password");
+
+
+ 
