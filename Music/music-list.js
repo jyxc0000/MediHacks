b15d01ab01cd2964da0413610a31f661131ac6df
ray060708 <ray060708@gmail.com>
2023-09-09 21:46:08 -0700

    a

---
+++ Music/music-list.js
@@ -0,0 +1,32 @@
+let allMusic = [
+    {
+        name: "Chill Lofi",
+        artist: "unknown",
+        img: "lofi",
+        src: "lofi"
+    },
+    {
+        name: "Smooth Jazz",
+        artist: "unknown",
+        img: "jazz",
+        src: "jazz"
+    },
+    {
+        name: "Classical Music",
+        artist: "unknown",
+        img: "classical",
+        src: "classical"
+    },
+    {
+        name: "Rap Mix",
+        artist: "Assorted",
+        img: "rap",
+        src: "rap"
+    },
+    {
+        name: "Pop Mix",
+        artist: "Assorted",
+        img: "pop",
+        src: "pop"
+    }
+]
